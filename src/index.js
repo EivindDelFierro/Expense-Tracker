73d52765af1fb9c112640ee103908854c559c5ab
Eivind Del Fierro <Eivind.Del.Fierro@gmail.com>
2024-02-10 08:47:22 -0500

    Feature: created and rendered App Component. Removed icons and references to icons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
+import App from "./app";
 
 import "./style.css";
-import pic from "./icon.png";
 
 document.addEventListener("DOMContentLoaded", () => {
   // establish an app element for React library to hook on to
@@ -12,5 +12,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // create a React root from the app element
   const root = createRoot(document.querySelector("#app"));
-  root.render(<h1>Hello, world</h1>);
+  root.render(<App />);
 });
